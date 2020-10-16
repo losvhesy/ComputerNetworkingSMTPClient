7bb0225c1c94528d8897688e62f81b0781dfea44
losvhesy <31491601+losvhesy@users.noreply.github.com>
2020-10-16 00:12:56 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -15,7 +15,6 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    print(recv)
     #if recv[:3] != '220':
         #print('220 reply not received from server.')
 
